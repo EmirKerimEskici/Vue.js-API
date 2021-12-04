1fbb0fbe1349e504f751ed1051b9135c404527aa
Emir <86002529+EmirKerimEskici@users.noreply.github.com>
2021-12-04 15:28:24 +0300

    Simple APİ
    Don’t forget to install `npm install axios`

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="App">
+  <img alt="Vue logo" src="./assets/api12.jpeg" class="api">
+  <users></users>
+  </div>
+</template>
+<script>
+import users from './components/users'
+export default {
+  name: 'App',
+  components: {
+    users,
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.api{
+  width: 500px;
+  margin-bottom: 55px;
+}
+</style>
---
+++ src/components/users.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="container">
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Username</th>
+      <th scope="col">e-Mail</th>
+      <th scope="col">Phone</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="user in users" :key="user.id"> <!--basit bir şekilde id'leri for ile döndürdüm -->
+      <th scope="row">{{ user.id }}</th>
+      <td>{{user.name}}</td>
+      <td>{{user.username}}</td>
+      <td>{{user.email}}</td>
+      <td>{{user.phone}}</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+data(){
+    return{
+        users:null
+    }},
+created: function(){
+    axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+        this.users = response.data;
+        })
+}
+}
+</script>
+<style>
+</style>
